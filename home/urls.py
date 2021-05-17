de1d987c4ceb3d16d6b53f3f51d9b598cb0b2b7a
vaibhavd <vaibhavdubewar28800@gmail.com>
2021-05-17 11:17:07 +0530

    Authentication

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path("",views.index, name='home'),
     path("about",views.about, name='about'),
     path("services",views.services, name='services'),
-    path("contact",views.contact, name='contact')
+    path("contact",views.contact, name='contact'),
+    path("signup",views.handleSignUp, name='handleSignUp'),
+    path("login",views.handleLogin, name='handleLogin'),
+    path("logout",views.handleLogout, name='handleLogout')
 ]
